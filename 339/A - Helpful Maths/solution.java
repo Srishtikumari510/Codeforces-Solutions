c4c9c7183618cccb2b452cdde363db8db14065d6
SRISHTI KUMARI <125398898+Srishtikumari510@users.noreply.github.com>
2026-07-30 00:02:42 +0530

    Add Helpful Maths [A] from Codeforces

---
+++ 339/A - Helpful Maths/solution.java
@@ -0,0 +1,24 @@
+import java.util.*;
+public class Main
+{
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        String a=sc.next();
+        
+        String arr[]=a.split("\\+");
+        
+        Arrays.sort(arr);
+        
+        for(int i=0;i<arr.length;i++){
+        System.out.print(arr[i]);
+        if(i!=arr.length-1)
+        {
+        System.out.print("+");
+ 
+        }
+        System.out.print("");
+        }
+        
+    }
+}
