87a0ed673f80eac712b8cbe6a7a036c6795c0112
SRISHTI KUMARI <125398898+Srishtikumari510@users.noreply.github.com>
2026-08-22 22:11:58 +0530

    Add Football [A] from Codeforces

---
+++ 96/A - Football/solution.java
@@ -0,0 +1,22 @@
+import java.util.*;
+ 
+public class Main
+{
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        String a=sc.next();
+        
+ 
+         
+          if(a.contains("1111111")||a.contains("0000000"))
+          {
+              System.out.println("YES");
+          }
+          else
+          {
+                 System.out.println("NO");
+ 
+          }
+    }
+}
